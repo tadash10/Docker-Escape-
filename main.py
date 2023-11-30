0f673f7ee435f10869c4e6cf4b58ca2a75ab8d4c
T1 <126980610+tadash10@users.noreply.github.com>
2023-11-30 00:47:47 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,160 @@
+import os
+import subprocess
+import logging
+from datetime import datetime
+
+def setup_logging():
+    log_filename = f'docker_escape_{datetime.now().strftime("%Y%m%d%H%M%S")}.log'
+    logging.basicConfig(filename=log_filename, level=logging.DEBUG, format='%(asctime)s [%(levelname)s]: %(message)s')
+
+def print_menu():
+    print("Advanced Docker Escape Attempt Menu:")
+    print("1. Perform Privilege Escalation Checks")
+    print("2. Verify Security Context")
+    print("3. Configure nsenter Options")
+    print("4. Exploit Vulnerabilities")
+    print("5. Enhance Logging")
+    print("6. Perform Post-Escape Analysis")
+    print("7. Generate Report")
+    print("8. Exit")
+
+def get_user_choice():
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-8): "))
+            if 1 <= choice <= 8:
+                return choice
+            else:
+                print("Invalid choice. Please enter a number between 1 and 8.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 8.")
+
+def privilege_escalation_checks():
+    try:
+        # Placeholder for various privilege escalation checks
+        # Add any additional checks that might lead to elevated privileges
+        logging.info("Performing privilege escalation checks...")
+
+        # Example: Check for sudo privileges
+        subprocess.run(['sudo', '-l'], check=True)
+        logging.info("User has sudo privileges.")
+
+        # Additional checks can be added here
+
+    except subprocess.CalledProcessError:
+        logging.warning("Privilege escalation checks failed. User may not have sudo privileges.")
+
+def security_context_verification():
+    try:
+        # Placeholder for checking security mechanisms (AppArmor, SELinux, etc.)
+        # Provide detailed information about the security context
+        logging.info("Verifying security context within the container and on the host...")
+
+        # Example: Check for AppArmor
+        apparmor_status = subprocess.run(['apparmor_status'], check=True, stdout=subprocess.PIPE, text=True).stdout
+        logging.info(f"AppArmor Status:\n{apparmor_status}")
+
+        # Example: Check for SELinux
+        selinux_status = subprocess.run(['sestatus'], check=True, stdout=subprocess.PIPE, text=True).stdout
+        logging.info(f"SELinux Status:\n{selinux_status}")
+
+    except subprocess.CalledProcessError:
+        logging.warning("Error while checking security context.")
+
+def configure_nsenter():
+    try:
+        # Placeholder for configuring nsenter options
+        # Explain the purpose and potential risks of each configuration
+        logging.info("Configuring nsenter options...")
+
+        # Example: Configure nsenter options
+        subprocess.run(['echo', 'kernel.unprivileged_userns_clone=1', '|', 'sudo', 'tee', '-a', '/etc/sysctl.conf'], check=True)
+        subprocess.run(['sudo', 'sysctl', '-p'], check=True)
+
+    except subprocess.CalledProcessError:
+        logging.warning("Error while configuring nsenter options.")
+
+def exploit_vulnerabilities():
+    # Placeholder for exploiting potential vulnerabilities in the host system
+    # Include explanations of the vulnerabilities and their exploitation
+    logging.info("Exploiting potential vulnerabilities in the host system...")
+
+    # Additional exploitation steps can be added here
+
+def enhance_logging():
+    # Placeholder for improving logging with detailed timestamps and context
+    logging.info("Enhancing logging with timestamps and detailed context...")
+
+    # Additional logging enhancements can be added here
+
+def post_escape_analysis():
+    # Placeholder for performing post-escape analysis
+    # Gather detailed information about the host system post-escape
+    logging.info("Performing post-escape analysis...")
+
+    # Additional post-escape analysis steps can be added here
+
+def generate_report():
+    # Function to generate a detailed report of the Docker escape attempt
+    report_filename = f'report_{datetime.now().strftime("%Y%m%d%H%M%S")}.txt'
+    with open(report_filename, 'w') as report_file:
+        report_file.write("Docker Escape Attempt Report\n")
+        report_file.write("===========================\n\n")
+
+        report_file.write("1. Privilege Escalation Checks:\n")
+        report_file.write("   - [ ] Detailed summary of checks performed\n\n")
+
+        report_file.write("2. Security Context Verification:\n")
+        report_file.write("   - [ ] AppArmor Status:\n")
+        report_file.write("   - [ ] SELinux Status:\n\n")
+
+        report_file.write("3. nsenter Configuration:\n")
+        report_file.write("   - [ ] Detailed explanation of nsenter configurations\n\n")
+
+        report_file.write("4. Vulnerability Exploitation:\n")
+        report_file.write("   - [ ] Exploited vulnerabilities and their impact\n\n")
+
+        report_file.write("5. Post-Escape Analysis:\n")
+        report_file.write("   - [ ] Information gathered about the host post-escape\n\n")
+
+        report_file.write("6. Recommendations:\n")
+        report_file.write("   - [ ] Security measures to prevent future breaches\n\n")
+
+    logging.info(f"Report generated: {report_filename}")
+
+def display_disclaimer():
+    print("Disclaimer:")
+    print("This script is intended for educational and authorized testing purposes only.")
+    print("Do not use this script in a production environment or without proper authorization.")
+    print("The creator of this script is not responsible for any misuse or damage caused by its execution.")
+
+def main():
+    setup_logging()
+    logging.info("Welcome to the Advanced Docker Escape Attempt!")
+
+    display_disclaimer()
+
+    while True:
+        print_menu()
+        choice = get_user_choice()
+
+        if choice == 1:
+            privilege_escalation_checks()
+        elif choice == 2:
+            security_context_verification()
+        elif choice == 3:
+            configure_nsenter()
+        elif choice == 4:
+            exploit_vulnerabilities()
+        elif choice == 5:
+            enhance_logging()
+        elif choice == 6:
+            post_escape_analysis()
+        elif choice == 7:
+            generate_report()
+        elif choice == 8:
+            logging.info("Exiting the Advanced Docker Escape Attempt. Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()
